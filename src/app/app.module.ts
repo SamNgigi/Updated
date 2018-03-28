25aad19d6f4eb6078d88df8fdfe8d1469050b109
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-28 16:12:13 +0300

    loading animation working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ProjectComponent } from './project/project.component';
+import { LoaderComponent } from './loader/loader.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
-    ProjectComponent
+    ProjectComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule
